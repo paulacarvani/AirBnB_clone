c423aeab0e9f5d789dec134a203caa47faca6123
paulacarvani <3312@holbertonschool.com>
2021-11-06 13:03:11 -0500

    REview style

--- models/base_model.py
+++ models/base_model.py
@@ -7,30 +7,42 @@ from datetime import datetime
 
 
 class BaseModel:
-  def __init__(self, *args, **kwargs):
-    self.id = str(uuid4())
-    self.created_at = datetime.today()
-    self.update_at = datetime.today()
+    """class BaseModel that defines all common attributes/methods
+    for other classes"""
 
-    format = "%Y-%m-%dT%H:%M:%S.%f"
+    def __init__(self, *args, **kwargs):
+        """Public instance attributes"""
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
 
-    if len(kwargs) != 0:
-      for ky, val in kwargs.items():
-        if ky == "created_at" or ky == "updated_at":
-          self.__dict__[ky] = datetime.strptime(val, format)
-        else:
-          self.__dict__[ky] =  val
+        format = "%Y-%m-%dT%H:%M:%S.%f"
 
-  def save(self):
-    self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            """Conditionals for kwargs"""
+            for ky, val in kwargs.items():
+                if ky == "created_at" or ky == "updated_at":
+                    self.__dict__[ky] = datetime.strptime(val, format)
+                else:
+                    self.__dict__[ky] = val
 
-  def to_dict(self):
-    ret_dict = self.__dict__.copy() 
-    ret_dict["__class__"] = self.__class__.__name__
-    ret_dict["created_at"] = self.created_at.isoformat()
-    ret_dict["update_at"] = self.update_at.isoformat()
-    return ret_dict
+    def save(self):
+        """Public instance method
+        updates the public instance attribute updated_at
+        with the current datetime"""
+        self.updated_at = datetime.today()
 
-  def __str__(self):
-    classname = self.__class__.__name__
-    return "[{}] ({}) {}".format( classname,self.id, self.__dict__)
+    def to_dict(self):
+        """Public instance method
+        returns a dictionary containing all keys/values of
+        __dict__ of the instance"""
+        ret_dict = self.__dict__.copy()
+        ret_dict["__class__"] = self.__class__.__name__
+        ret_dict["created_At"] = self.created_at.isoformat()
+        ret_dict["updated_at"] = self.updated_at.isoformat()
+        return ret_dict
+
+    def __str__(self):
+        """should print: [<class name>] (<self.id>) <self.__dict__>"""
+        classname = self.__class__.__name__
+        return "[{}] ({}) {}".format(classname, self.id, self.__dict__)
