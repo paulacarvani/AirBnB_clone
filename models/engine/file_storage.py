ded09ad0ce7cc3fd5373b8419de27d03325d3e1a
Andrea Angola <3469@holbertonschool.com>
2021-11-07 15:25:57 -0800

    pep8 errors

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" class FileStorage that serializes instances to a JSON file and deserializes JSON file to instances"""
+""" class FileStorage that serializes instances
+to a JSON file and deserializes JSON file to instances"""
 from models.base_model import BaseModel
 import json
 
@@ -25,13 +26,14 @@ class FileStorage():
         for key, value in FileStorage.__objects.items():
             if not isinstance(value, dict):
                 FileStorage.__objects[key] = value.to_dict()
-        with open(FileStorage.__file_path, mode="w", encoding="utf-8") as json_file:
+        with open(FileStorage.__file_path, mode="w", encoding="utf-8")
+        as json_file:
             json.dump(FileStorage.__objects, json_file)
 
     def reload(self):
         """deserializes the JSON file to __objects (only if the JSON file
-        (__file_path) exists ; otherwise, do nothing. If the file doesn’t exist,
-        no exception should be raised)"""
+        (__file_path) exists ; otherwise, do nothing.
+        If the file doesn’t exist, no exception should be raised)"""
         try:
             with open(FileStorage.__file_path, "r") as json_file:
                 dir_obj = json.load(json_file)
