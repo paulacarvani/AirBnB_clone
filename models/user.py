a48ed29cc8826773c7e13cb063a8cf84b68408f3
Andrea Angola <3469@holbertonschool.com>
2021-11-07 18:29:28 -0800

    class user

---
+++ models/user.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""class User that inherits from BaseModel"""
+from models.base_models import BaseModel
+
+
+class User(BaseModel):
+    """Class User Public class attributes"""
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
