76c91420e527c1ac7a5799c9662ec9483744dd3c
paulacarvani <3312@holbertonschool.com>
2021-11-07 23:06:37 -0500

    Update file base

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         """ initializes class with id and created at time"""
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.today()
         self.updated_at = datetime.today()
 
