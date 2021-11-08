b5b5b14d49bcdec3bc8a44038da44513892eea86
Andrea Angola <3469@holbertonschool.com>
2021-11-07 18:19:28 -0800

    class City

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class User that inherits from BaseModel"""
+from models.base_models import BaseModel
+
+
+class Amenity(BaseModel):
+    """Class Amenity Public class attributes"""
+    name = ""
---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""class City that inherits from BaseModel"""
+from models.base_models import BaseModel
+
+
+class City(BaseModel):
+    """Class City Public class attributes"""
+    state_id = ""
+    name = ""
