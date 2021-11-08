9032e10f47e6ba696226a940d02130068dcf3330
Andrea Angola <3469@holbertonschool.com>
2021-11-07 18:27:22 -0800

    class state

---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class State that inherits from BaseModel"""
+from models.base_models import BaseModel
+
+
+class State(BaseModel):
+    """Class State Public class attributes"""
+    name = ""
